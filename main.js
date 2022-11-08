05948b365b24d1055cb8effc22d5b279d57f7371
kiTheKoder <mitchellkiana@gmail.com>
2022-11-07 23:29:22 -0600

    final

--- main.js
+++ main.js
@@ -28,15 +28,48 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
-  // your code here
+const generateHint = (guess) =>  {
+  //splits the user guess and solution into an array
+  let hintArray = solution.split('');
+  let guessArray = guess.split('');
+
+  //holds value for hint
+  let correctLocation = 0;
+
+  //loops through each array
+  for (let i=0; i < hintArray.length; i++) {
+    for (let j=0; j < guessArray.length; j++) {
+
+      //checks for equality at matching indexes of the array
+      if (hintArray[i] === guessArray[i]) {
+        correctLocation++;
+        hintArray[i] = null;
+      } 
+    }
+  }
+  let correctSolution = 0
+
+  // checks for correct letter not in correct location
+  for (let i=0; i < hintArray.length; i++) {
+    let loopedIndexes = hintArray.indexOf(guessArray[i]);
+    if (loopedIndexes > -1) {
+      correctSolution++;
+      hintArray[loopedIndexes] = null;
+    }
+  }
+  return correctLocation + '-' + correctSolution;
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
-}
+  // solution = 'abcd'; // Comment this out to generate a random solution
+  if (guess !== solution) {
+    console.log(board.push("Try again! Here's a hint:" + generateHint(guess)));
+ 
+  } else {
+    return "You guessed it!";
+  }
 
+}
 
 const getPrompt = () =>  {
   rl.question('guess: ', (guess) => {
